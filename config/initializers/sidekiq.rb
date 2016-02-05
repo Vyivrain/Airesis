3025906cc87afc1c26d8520d7de14885c7aa15c3
vyivrain <stranger9983@gmail.com>
2016-02-05 12:01:13 +0200

    Changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq::Logging.logger = Logger.new(Rails.root.join('log', 'sidekiq.log'), 50, 100.megabytes)
